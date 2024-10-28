21718efaf421fcbbb4d93bfe43229f57e0cf0ed6
Afsoon <aslani.a7260@gmail.com>
2024-10-28 13:53:19 +0330

    rename rendere file name

--- src/render-video.js
+++ src/render-video.js
@@ -6,7 +6,7 @@ const renderVideo = async () => {
   try {
     await renderMedia({
       serveUrl: "http://localhost:3000", // URL when Remotion is running
-      composition: "YourComposition", // Replace with your composition name
+      composition: "HelloWorld", // Replace with your composition name
       codec: "h264", // or 'vp8', 'vp9', 'h265'
       outputLocation: path.join(__dirname, "out", "video.mp4"), // Output path for the video
     });
